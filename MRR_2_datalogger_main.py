160c3bf4f4afc5b52dbd5e4876e9a1f3ee81dda7
Luis Ackermann <ackermann.luis@gmail.com>
2022-12-31 14:11:38 +1100

    Updated such that it can:
    export data to FTP daily (on new day)
    
    log status to a file instead of terminal
    
    works on linux and windows

--- MRR_2_datalogger_main.py
+++ MRR_2_datalogger_main.py
@@ -14,12 +14,63 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import serial
-import time
 import sys
+import glob
+import time
+import os
+import ftplib
+import serial
+from threading import Thread
+
+# ################################################  USER VARIABLES  #################################################
+# first port name to try
+port_ = '/dev/ttyUSB0'
+height_resolution = '150'
+
+# place an empty file named __findmeMRR__ inside this directory in the folder where you want the files to be saved
+output_path_base = '/media/pi/'
+
+# define file prefix
+file_prefix = 'MRR_W_'
+
+# if enabled, the data will be also sent to the specified ftp on the start of a new day
+enable_ftp_export = False
+ftp_hostname = ''
+ftp_username = ''
+ftp_password = ''
+ftp_path     = '/'
+# ################################################  USER VARIABLES  #################################################
 
 
+def ftp_put_file(local_filename, remote_path,
+                 host_name=None, user_name=None, password_=None):
+    from pathlib import Path
+    try:
+        local_file_path = Path(local_filename)
+
+        ftp = ftplib.FTP(host=host_name, user=user_name, passwd=password_)
+        ftp.cwd(remote_path)
+
+        with open(local_filename, 'rb') as local_file:
+            ftp.storbinary(f'STOR {local_file_path.name}', local_file)
+
+        ftp.quit()
+    except:
+        try:
+            ftp.quit()
+        except:
+            pass
+def ftp_get_file(remote_filename, local_path,
+                 host_name=None, user_name=None, password_=None):
+    from pathlib import Path
+    remote_file_path = Path(remote_filename)
+
+    ftp = ftplib.FTP(host=host_name, user=user_name, passwd=password_)
+
+    with open(local_path + remote_file_path.name, 'wb') as local_file:
+        ftp.retrbinary(f'RETR {remote_filename}', local_file.write)
+
+    ftp.quit()
 def list_files_recursive(path_, filter_str=None):
     # create list of raw spectra files
     file_list = []
@@ -36,15 +87,49 @@ def list_files_recursive(path_, filter_str=None):
                     filename_ = os.path.join(r, file)
                     file_list.append(filename_.replace('\\', '/'))
     return sorted(file_list)
+def log_status(text_):
+    status_log_filename = output_path + 'status_log.txt'
+    if os.path.isfile(status_log_filename):
+        with open(status_log_filename, 'a') as file_:
+            file_.write(str(text_) + '\n')
+    else:
+        with open(status_log_filename, 'w') as file_:
+            file_.write(str(text_) + '\n')
+def list_available_serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/ttyUSB[A-Za-z]*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
 
-output_path = '/home/mrr/Data_MRR_1/MRR_'
-port_ = '/dev/ttyUSB0'
-height_resolution = '50'
+time.sleep(10)
+output_path = list_files_recursive(output_path_base, '__findmeMRR__')[0][:-13] + file_prefix
 
-com_ports_list = list_files_recursive('/dev/', 'ttyUSB')
+
+com_ports_list = list_available_serial_ports()
 error_encountered = False
 current_try_port_index = 0
+last_date = time.strftime('%Y%m%d', time.localtime())
 
 while True:
     try:
@@ -54,7 +139,7 @@ while True:
                                   bytesize=serial.EIGHTBITS,
                                   stopbits=serial.STOPBITS_ONE,
                                   )
-        print('initialized connection')
+        log_status('initialized connection')
 
         # flush full telegram
         txt = 'SN'.encode()
@@ -64,7 +149,7 @@ while True:
                 raise ValueError('hang while reading telegram, restarting connection')
             ibuffer = interface.read()
             txt += ibuffer
-        print('buffer cleaned')
+        log_status('buffer cleaned')
 
         # set height resolution
         heights_meters_dict={
@@ -85,7 +170,7 @@ while True:
         }
         set_height_resolution_command = b'\x02\x48\x53\x3d' + heights_meters_dict[height_resolution] + b'\x0a'
         interface.write(set_height_resolution_command)
-        print('height resolution set to', height_resolution, 'meters')
+        log_status('height resolution set to', height_resolution, 'meters')
 
         # flush full telegram
         txt = 'SN'.encode()
@@ -95,10 +180,10 @@ while True:
                 raise ValueError('hang while reading telegram, restarting connection')
             ibuffer = interface.read()
             txt += ibuffer
-        print('buffer cleaned')
+        log_status('buffer cleaned')
 
-        print('-' * 40)
-        print('starting acquisition loop')
+        log_status('-' * 40)
+        log_status('starting acquisition loop')
 
 
         while True:
@@ -133,39 +218,49 @@ while True:
                         for line_ in txt_lines_clean:
                             file_.write(line_)
                             file_.write('\n')
+                    
+                    if os.path.isfile(output_path + last_date + '.raw'):
+                        last_date = time.strftime('%Y%m%d', time.localtime())
 
+                        if enable_ftp_export:
+                            ftp_thread = Thread(target=ftp_put_file,
+                                                args=[output_path + last_date + '.csv',
+                                                      ftp_path, ftp_hostname, ftp_username, ftp_password])
+                            ftp_thread.run()
 
-                print(time.strftime('%Y-%m-%d_%H:%M:%S', time_), 'logged MRR data to file')
+                log_status(time.strftime('%Y-%m-%d_%H:%M:%S', time_), 'logged MRR data to file')
             else:
-                print('skipping time stamp as telegram was received incomplete')
+                log_status('skipping time stamp as telegram was received incomplete')
 
             time.sleep(3)
             error_encountered = False
 
     except BaseException as error_msg:
-        if error_encountered:
-            while True:
-                print('searching available ports')
-                com_ports_list = list_files_recursive('/dev/', 'ttyUSB')
-                if len(com_ports_list) > 0:
-                    if current_try_port_index >= len(com_ports_list) - 1:
-                        current_try_port_index = 0
-                    else:
-                        current_try_port_index += 1
-                    port_ = com_ports_list[current_try_port_index]
-                    break
-                else:
-                    time.sleep(3)
-
-        error_encountered = True
-        line_number = sys.exc_info()[-1].tb_lineno
-        print('error in line {0}, error message:\n{1}'.format(line_number, error_msg))
         try:
-            print('trying to close the connection to open it again (reset it)')
+
+            if error_encountered:
+                while True:
+                    log_status('searching available ports')
+                    com_ports_list = list_files_recursive('/dev/', 'ttyUSB')
+                    if len(com_ports_list) > 0:
+                        if current_try_port_index >= len(com_ports_list) - 1:
+                            current_try_port_index = 0
+                        else:
+                            current_try_port_index += 1
+                        port_ = com_ports_list[current_try_port_index]
+                        break
+                    else:
+                        time.sleep(3)
+
+            error_encountered = True
+            line_number = sys.exc_info()[-1].tb_lineno
+            log_status('error in line {0}, error message:\n{1}'.format(line_number, error_msg))
+
+            log_status('trying to close the connection to open it again (reset it)')
             time.sleep(5)
             interface.close()
             time.sleep(5)
-            print('successfully closed!')
+            log_status('successfully closed!')
         except:
             pass
 
